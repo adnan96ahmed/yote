191c39e0247f76f05ee46f822fa75543e84351bc
Grant <grantdouglashand@outlook.com>
2017-11-06 23:12:39 -0500

    initialize done, need a bit of clarification on some of their spec though

--- move.rb
+++ move.rb
@@ -9,6 +9,9 @@ class Move
 		@destination = destination
 		@playerColour = playerColour
 		@gameBoard = gameBoard
+		@move = 0
+
+
 	end
 
 	def source
@@ -16,16 +19,41 @@ class Move
 	end
 
 	def destination
-		return @source
+		return @destination
 	end
 
 	def playerColour
-		return @source
+		return @playerColour
 	end
 
 	def gameBoard
-		return @source
+		return @gameBoard
 	end
+
+	def boolcompate()
+
+	end
+
+	def validate()
+
+	end
+
+	def isPossibleMove()
+
+	end
+
+	def to_s()
+
+	end
+
+	def findMoveType(source, gameBoard, destination)
+		if source == NULL and gameBoard.atPosition() == 0
+			@move = :placement
+		else
+			@move = :illegal
+		end
+	end
+
 
 	# boolcompare(Move:lastMove)
 	# bool validate()
@@ -33,7 +61,7 @@ class Move
 	# bool isPossibleMove()
 	# string to_s()
 # ------------------------------------------------------------Private Funtions
-	# MoveType findMoveType() 
+	# MoveType findMoveType()
 	# 	x = 0, y = 1 # For easier reading
 	# 	jumped = [(@destination[x] + @source[x]) / 2, (@destination[y] + @source[y]) / 2]
 	# 	result = (@gameBoard.atPosition(@jumped) != :empty &&
@@ -41,7 +69,7 @@ class Move
 	# 	if result == true @move = :capture
 	# 		11
 	# 	else
-	# 		@move = :illegal 
+	# 		@move = :illegal
 	# 	end
 
 end
@@ -50,5 +78,6 @@ hi = [1, 2]
 bye = [3,4]
 no = [5,6]
 object = Move.new(hi, bye, "black", no)
+puts object.findMoveType()
 puts object.source
 
